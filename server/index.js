4fb5d03c53c6b5eb51c257f73785a226dddf419c
Hieu Tran Trung <trunghieu05092001@gmail.com>
2022-08-12 16:04:46 +0700

    Fix index

--- server/index.js
+++ server/index.js
@@ -36,5 +36,5 @@ const host = '0.0.0.0';
 const port = process.env.PORT || 5000;
 
 app.listen(port, host, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${port}`);
 });
