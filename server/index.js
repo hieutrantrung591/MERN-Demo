7ff10cac3c7bbc6a11e885a8fdd0b7c83842591a
Hieu Tran Trung <trunghieu05092001@gmail.com>
2022-08-12 16:02:57 +0700

    Fix index

--- server/index.js
+++ server/index.js
@@ -35,6 +35,6 @@ app.use("/api/posts", postRouter);
 const host = '0.0.0.0';
 const port = process.env.PORT || 5000;
 
-app.listen(port, host, "localhost", () => {
+app.listen(port, host, () => {
   console.log(`Server running on port ${PORT}`);
 });
